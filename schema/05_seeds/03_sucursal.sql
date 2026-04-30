59b32f99ddbd5e67a887e080745d274f53344b05
Giuliano Hillebrand <giulianohillebrand@gmail.com>
2026-04-30 17:44:33 -0300

    Prueba github.dev

--- schema/05_seeds/03_sucursal.sql
+++ schema/05_seeds/03_sucursal.sql
@@ -3,4 +3,5 @@ INSERT INTO sucursal (nombre, direccion, ciudad, telefono) VALUES
     ('Palermo',    'Av. Santa Fe 3500',         'CABA',       '011-5000-2000'),
     ('San Isidro', 'Av. del Libertador 16000',  'San Isidro', '011-5000-3000'),
     ('Rosario',    'Bv. Orono 800',             'Rosario',    '0341-500-4000'),
+    ('Posadas',     'Av. Blas Parera 1232',     'Posadas'       '0376-456-3212'),
     ('Cordoba',    'Av. Colon 500',             'Cordoba',    '0351-500-5000');
