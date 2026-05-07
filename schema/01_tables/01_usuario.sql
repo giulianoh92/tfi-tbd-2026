9365911b22f597bad38b0e37b8f38a329369ccad
Giuliano Hillebrand <giulianohillebrand@gmail.com>
2026-05-07 16:47:29 -0300

    pinga revert

--- schema/01_tables/01_usuario.sql
+++ schema/01_tables/01_usuario.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS usuario (
     username        VARCHAR(50)  NOT NULL UNIQUE,
     password_hash   VARCHAR(255) NOT NULL,
     email           VARCHAR(150) NOT NULL UNIQUE,
-    created_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMPgfhfjhfjh
+    created_at      TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
